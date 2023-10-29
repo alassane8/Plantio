adac63b613a636e3b0223e5dc2f07834c6f99962
alassane8 <alassane.wade@edu.ece.fr>
2023-10-29 19:28:29 +0100

    feat: new pages need work

--- mobile-app/pages/about.js
+++ mobile-app/pages/about.js
@@ -3,17 +3,19 @@ import NavigationBar from "../component/navbar";
 
 export default function about() {
   return (
+    
   <div>
     <div className="bg-gray-100 h-screen flex flex-col items-center">
       <h1 className="text-3xl font-extrabold text-indigo-600 mb-4">
         About Us
       </h1>
-      <Link href="/home">
+      
+  <NavigationBar />
+      <Link href="/login">
         <button className="bg-gray-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full mt-4 transition duration-300 ease-in-out transform hover:scale-105">
         Home Page
         </button>
       </Link>
-
     </div>
   </div>
   )
--- mobile-app/pages/home.js
+++ mobile-app/pages/home.js
@@ -3,18 +3,9 @@ import NavigationBar from "../component/navbar";
 import Header from '../component/header';
 import Footer from '../component/footer';
 
-export default function login() {
+export default function home() {
+
   return (
-    <div>
-        <NavigationBar />
-        <div className="bg-gray-100 h-screen flex flex-col items-center">
-            <Header />
-            <h1 className="text-5xl font-bold text-blue-600 mb-4">Welcome to the Home Page</h1>
-            <div className="border border-gray-300 p-4 rounded-lg">
-                <p className="text-gray-600">More content goes here...</p>
-            </div>      
-        <Footer />
-        </div>
-    </div>
+    <NavigationBar />
   )
 }
