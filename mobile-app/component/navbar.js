73bd006d1701b4b3c5a6ef35611f164b51c06c92
alassane8 <alassane.wade@edu.ece.fr>
2023-10-30 12:36:25 +0100

    feat: modifications navbar.js

--- mobile-app/component/navbar.js
+++ mobile-app/component/navbar.js
@@ -1,20 +1,41 @@
 import Link from "next/link";
-import Header from '../component/header';
-import Footer from '../component/footer';
 
 export default function NavigationBar() {
   return (
-    <nav className="bg-pink-400 p-4">
+    <nav className="bg-gary-700 p-4">
         <div className="border border-blue-300 p-4 rounded-lg">
             <div className="space-x-4">
-                <aside class="w-64 bg-gray-800 text-white p-4">
-                    <h2 class="text-2xl font-semibold">Sidebar</h2>
+                <aside class="w-24 bg-gray-800 text-white p-4">
                     <ul class="mt-4 space-y-2">
-                    <li><a href="/about" class="hover:text-blue-400">About us</a></li>
-                    <li><a href="/contacts"class="hover:text-blue-400">Contatcts</a></li>
-                    <li><a href="/home" class="hover:text-blue-400">Home</a></li>
-                    <li><a href="/login" class="hover:text-blue-400">login</a></li>
-                    </ul>
+                    <li>
+                      <Link href="/createAccount">
+                        <button className ="hover:bg-pink-400 text-white font-bold py-2 px-2 mt-4 rounded-full transition duration-500 ease-in-out transform hover:scale-105">
+                          Create Account
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/login">
+                        <button className ="hover:bg-pink-400 text-white font-bold py-2 px-2 mt-4 rounded-full transition duration-500 ease-in-out transform hover:scale-105">
+                          Login
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/about">
+                        <button className ="hover:bg-pink-400 text-white font-bold py-2 px-2 mt-4 rounded-full transition duration-500 ease-in-out transform hover:scale-105">
+                          About us
+                        </button>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link href="/contacts">
+                        <button className ="hover:bg-pink-400 text-white font-bold py-2 px-2 mt-4 rounded-full transition duration-500 ease-in-out transform hover:scale-105">
+                          Contact us
+                        </button>
+                      </Link>
+                    </li>
+                  </ul>
                 </aside>
             </div>
         </div>
